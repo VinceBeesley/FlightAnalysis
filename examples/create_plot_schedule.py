8e9d7eec2d30b42bdfe131adff900ff76c7f3944
Thomas David <thomasdavid0@gmail.com>
2021-07-30 14:01:41 +0100

    updated f21

--- examples/create_plot_schedule.py
+++ examples/create_plot_schedule.py
@@ -4,13 +4,11 @@ from examples.model import obj
 
 
 
-#temp = p21.scale_distance(170).create_raw_template("left", 30.0, 170)
-#plotsec(temp, obj, 10, 10).show()
-#
-#plotsec(p21.manoeuvres[5].get_data(temp), obj, 10, 10).show()
+temp = p21.scale_distance(170).create_raw_template("left", 30.0, 170)
+
+plotsec(p21.manoeuvres[5].get_data(temp), obj, 5, 10).show()
 
 temp = f21.scale_distance(170).create_raw_template("left", 30.0, 170)
-plotsec(temp, obj, 10, 10).show()
 
-plotsec(f21.manoeuvres[5].get_data(temp), obj, 10, 10).show()
+plotsec(f21.manoeuvres[5].get_data(temp), obj, 5, 10).show()
 
