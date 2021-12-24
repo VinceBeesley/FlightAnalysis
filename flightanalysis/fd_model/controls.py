6c237c8fa6ce94ff1660d69b8030315b0a81d732
Thomas David <thomas.david@bristol.ac.uk>
2021-12-24 15:50:45 +0000

    controls

--- flightanalysis/fd_model/controls.py
+++ flightanalysis/fd_model/controls.py
@@ -1,12 +1,8 @@
 import numpy as np
 import pandas as pd
 from geometry.factory import geoms_factory, geom_factory
-<<<<<<< HEAD
-    
-=======
 from typing import Union
 
->>>>>>> devb
 
 names = ["throttle", "aileron", "elevator", "rudder", "flap"]
 
