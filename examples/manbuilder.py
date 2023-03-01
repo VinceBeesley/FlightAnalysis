9f03905b1d385e2d7ff54429c7cbfaa19c01516e
thomas david <thomasdavid0@gmail.com>
2023-03-01 19:12:30 +0000

    misc

--- examples/manbuilder.py
+++ examples/manbuilder.py
@@ -4,28 +4,19 @@ from flightanalysis.schedule.definition.manoeuvre_builder import f3amb
 from flightanalysis.criteria import *
 
 
-mdef = f3amb.create(ManInfo("Humpty Bump",  "hB2",  3, Position.END,
-            BoxLocation(Height.BTM, Direction.UPWIND, Orientation.UPRIGHT),
-            BoxLocation(Height.BTM)
+mdef = f3amb.create(ManInfo(
+            "Top Hat", "tHat", k=4, position=Position.CENTRE, 
+            start=BoxLocation(Height.BTM, Direction.UPWIND, Orientation.UPRIGHT),
+            end=BoxLocation(Height.BTM)
         ),[
             f3amb.loop(np.pi/2),
-            f3amb.roll("roll_option[0]"),
-            f3amb.loop(np.pi),
-            f3amb.roll("roll_option[1]"),
-            f3amb.loop(-np.pi/2)   
-        ], 
-        roll_option=ManParm("roll_option", Combination(
-            [
-                [np.pi, np.pi],
-                [np.pi, -np.pi],
-                [-np.pi, np.pi],
-                [-np.pi, -np.pi],
-                [np.pi*1.5, -np.pi/2], 
-                [-np.pi*1.5, np.pi/2]
-            ]), 0
-        )
-    )
-
+            f3amb.roll("2x4"),
+            f3amb.loop(np.pi/2), 
+            f3amb.roll("1/2",line_length=100),
+            f3amb.loop(-np.pi/2),
+            f3amb.roll("2x4"),
+            f3amb.loop(-np.pi/2)
+        ])
 
 it = mdef.info.initial_transform(170, 1)
 man = mdef.create(it)
