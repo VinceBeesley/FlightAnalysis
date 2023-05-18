fixes for when quaternion data is missing

flightanalysis/flightline.py
@@ -10,7 +10,7 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from geometry import GPS, Coord, Point, Transformation, Quaternion, PX, PY, PZ, P0
+from geometry import GPS, Coord, Point, Transformation, Quaternion, PX, PY, PZ, P0, Euler
 from typing import Union
 from flightdata import Flight, Fields
 import numpy as np
@@ -27,6 +27,7 @@ class Box(object):
         self.country=country
         self.pilot_position = pilot_position
         self.heading = heading
+        self.rotation = Euler(0, 0, -self.heading)
 
     def to_dict(self) -> dict:
         temp = self.__dict__.copy()
@@ -63,11 +64,10 @@ class Box(object):
         imu_ready_data = flight.data.loc[flight.imu_ready_time()]
 
         position = GPS(*imu_ready_data[Fields.GLOBALPOSITION.names])
-        heading = Quaternion(
-            *imu_ready_data[Fields.QUATERNION.names]
+        heading = Euler(
+            *imu_ready_data[Fields.ATTITUDE.names]
         ).transform_point(PX())
-        
-        
+
         return Box('origin', position[0], np.arctan2(heading.y, heading.x)[0], "unknown", "unknown")
 
     @staticmethod
@@ -120,6 +120,15 @@ class Box(object):
         )
 
 
+    def gps_to_point(self, gps: GPS) -> Point:
+        pned = gps - self.pilot_position
+        return self.rotation.transform_point(Point(pned.y, pned.x, -pned.z ))
+
+
+#    def point_to_gps(self, pos: Point) -> GPS:
+ #       return self.pilot_position + self.rotation.inverse().transform_point(pos)
+    
+
 class FlightLine(object):
     '''class to define where the flight line is in relation to the raw input data
     It contains two coordinate frames (generally used for reference / debugging only) and two transformations, 
@@ -146,7 +155,8 @@ class FlightLine(object):
 
     @staticmethod
     def home():
-        return FlightLine(Coord.from_nothing(), Coord.from_nothing())
+        """Default home is NWU"""
+        return FlightLine(Coord.from_nothing(), Coord.from_zx(P0(), PZ(-1), PX()))
 
     @staticmethod
     def from_box(box: Box, world_home: GPS):

tests/test_flightline.py
@@ -2,7 +2,8 @@ import unittest
 
 from flightdata.data import Flight, Fields
 from flightanalysis.flightline import FlightLine, Box
-from geometry import GPS, Point, Quaternion, PX, PY, PZ, P0
+from geometry import GPS, Point, Quaternion, PX, PY, PZ, P0, Euler
+from geometry.testing import assert_almost_equal
 from math import pi, cos, sin
 import numpy as np
 
@@ -171,6 +172,40 @@ def test_from_f3a_zone():
     box = Box.from_f3a_zone("tests/test_inputs/tl_2_box.f3a")
     assert box.pilot_position == GPS(52.5422769, -1.6318313)
     assert np.degrees(box.heading) == approx(75.281, 1e-3)
+
+
+
+def test_box_gps_to_point_north():
+    home = GPS(39, -105)
+    tests = Point.concatenate([
+        PX(10),
+        PY(10),
+        Point(10, 10, 0)
+    ])
+    p = home.offset(tests)
+
+    # Box heading specified in radians from North (clockwise)
+    box = Box('north', home, 0)
+
+    assert_almost_equal(box.gps_to_point(p), tests)
+
+def test_box_gps_to_point_not_north():
+    home = GPS(39, -105)
+    tests = Point.concatenate([
+        PX(10),
+        PY(10),
+        Point(10, 10, 0)
+    ])
+    rot = np.degrees(75)
+    p = home.offset(tests)
+
+    # Box heading specified in radians from North (clockwise)
+    box = Box('north', home, rot)
+
+    assert_almost_equal(box.gps_to_point(p), Euler(0, 0, rot).transform_point(tests))
+
+
+
 
 
 if __name__ == "__main__":

tests/test_state/test_state_builders.py
@@ -1,9 +1,10 @@
 from flightanalysis.state.state import State
+from flightanalysis.flightline import Box
 from flightanalysis.state.tools.builders import extrapolate, from_flight
-
+from flightdata import Flight
 from flightanalysis.base.table import Time
 from pytest import approx, fixture, raises
-from geometry import Transformation, PX, PY, P0
+from geometry import Transformation, PX, PY, P0, Point
 import numpy as np
 from ..conftest import flight, box
 from time import sleep, time
@@ -73,3 +74,8 @@ def test_stack_singles():
     assert time()-start == approx(st.duration, abs=1e-2)
 
 
+def test_from_fl():
+    fl = Flight.from_csv("tests/test_inputs/00000129.csv")
+    st = State.from_flight(fl, Box.from_initial(fl))
+    
+    assert not np.any(np.isnan(st.pos.data))